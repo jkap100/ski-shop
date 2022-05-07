b020eb5eea567f2f2cca12e814963a731f68944b
Jon Kaplon <jon.kaplon1@gmail.com>
2022-05-07 10:24:04 -0700

    adds accessory relation

--- app/controllers/user_accessories_controller.rb
+++ app/controllers/user_accessories_controller.rb
@@ -1,2 +1,20 @@
 class UserAccessoriesController < ApplicationController
+
+    skip_before_action :authorize
+
+    def index
+        render json: UserAccessory.all
+    end
+
+    def create
+        user_accessory = UserAccessory.create(user_accessory_params)
+        render json: user_accessory, status: :created
+    end
+
+    private
+
+    def user_accessory_params
+        params.permit(:user_id, :accessory_id)
+    end
+
 end
