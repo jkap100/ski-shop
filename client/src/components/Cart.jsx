89fcff5268b55f72f430bcbf16c4706605584a1a
Jon Kaplon <jon.kaplon1@gmail.com>
2022-05-08 08:14:33 -0700

    adds accessory subtotal to cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,16 +1,75 @@
 import React from "react";
-import CartAccessories from "./CartAccessories";
 
 function Cart({ cartObj }) {
-  console.log(cartObj);
-  const cartItems = cartObj.accessories.map((c) => (
-    <CartAccessories key={c.id} cartAccessories={c} />
-  ));
+  //   console.log(cartObj);
+
+  const cartAccessories = cartObj.accessories.map(
+    (accessory) => accessory.price
+  );
+
+  const initialValue = 0;
+  const totalAccessoryPrice = cartAccessories.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  console.log(totalAccessoryPrice);
 
   return (
-    <div>
-      <div>Accessories</div>
-      {cartItems}
+    <div className="container">
+      <div className="column">
+        <h3 className="title is-4">Accessories</h3>
+        <table className="table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartObj.accessories.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.category}</td>
+                <td>
+                  <button className="button is-black is-small">X</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h3 className="total">
+          <strong>Subtotal Accessories : ${totalAccessoryPrice}</strong>
+        </h3>
+      </div>
+      <div className="column">
+        <h3 className="title is-4">Accessories</h3>
+        <table className="table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartObj.accessories.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.category}</td>
+                <td>
+                  <button className="button is-black is-small">X</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h3 className="total">Total : /></h3>
+      </div>
     </div>
   );
 }
--- client/src/components/CartAccessories.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CartAccessories({ cartAccessories }) {
-  return <div>{cartAccessories.name}</div>;
-}
-
-export default CartAccessories;
