9d0ac542fdf4d068552d8823cb6266b86ce8de2a
Jon Kaplon <jon.kaplon1@gmail.com>
2022-05-08 08:57:11 -0700

    adds styling and total to cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -7,68 +7,127 @@ function Cart({ cartObj }) {
     (accessory) => accessory.price
   );
 
+  const cartApparels = cartObj.apparels.map((apparel) => apparel.price);
+
+  const cartSkis = cartObj.skis.map((ski) => ski.price);
+
   const initialValue = 0;
   const totalAccessoryPrice = cartAccessories.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     initialValue
   );
-  console.log(totalAccessoryPrice);
+  const totalApparelPrice = cartApparels.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  const totalSkiPrice = cartApparels.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+
+  const grandTotal = totalSkiPrice + totalApparelPrice + totalAccessoryPrice;
 
   return (
     <div className="container">
       <div className="column">
-        <h3 className="title is-4">Accessories</h3>
-        <table className="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>Item Name</th>
-              <th>Price</th>
-              <th>Category</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartObj.accessories.map((item, index) => (
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.category}</td>
-                <td>
-                  <button className="button is-black is-small">X</button>
-                </td>
+        <div className="box">
+          <table className="table is-fullwidth">
+            <thead>
+              <tr>
+                <th className="product">Item Name</th>
+                <th className="price">Price</th>
+                <th className="category">Category</th>
+                <th>Remove</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <h3 className="total">
-          <strong>Subtotal Accessories : ${totalAccessoryPrice}</strong>
-        </h3>
+            </thead>
+            <tbody>
+              {cartObj.skis.map((item, index) => (
+                <tr key={index}>
+                  <td className="product">{item.name}</td>
+                  <td className="price">{item.price}</td>
+                  <td className="category">{item.category}</td>
+                  <td>
+                    <button className="button is-black is-small">X</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="box has-background-black">
+            <h3 className="title is-4 has-text-white">
+              Subtotal - Skis : ${totalSkiPrice}
+            </h3>
+          </div>
+        </div>
       </div>
+
       <div className="column">
-        <h3 className="title is-4">Accessories</h3>
-        <table className="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>Item Name</th>
-              <th>Price</th>
-              <th>Category</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartObj.accessories.map((item, index) => (
-              <tr key={index}>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.category}</td>
-                <td>
-                  <button className="button is-black is-small">X</button>
-                </td>
+        <div className="box">
+          <table className="table is-fullwidth">
+            <thead>
+              <tr>
+                <th className="product">Item Name</th>
+                <th className="price">Price</th>
+                <th className="category">Category</th>
+                <th>Remove</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <h3 className="total">Total : /></h3>
+            </thead>
+            <tbody>
+              {cartObj.apparels.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.category}</td>
+                  <td>
+                    <button className="button is-black is-small">X</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="box has-background-black">
+            <h3 className="title is-4 has-text-white">
+              Subtotal - Apparel : ${totalApparelPrice}
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="column">
+        <div className="box">
+          <table className="table is-fullwidth">
+            <thead>
+              <tr>
+                <th className="product">Item Name</th>
+                <th className="price">Price</th>
+                <th className="category">Category</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartObj.accessories.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
+                  <td>{item.category}</td>
+                  <td>
+                    <button className="button is-black is-small">X</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="box has-background-black">
+            <h3 className="title is-4 has-text-white">
+              Subtotal - Accessories : ${totalAccessoryPrice}
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className="box">
+        <h3 className="title is-4 has-text-black">
+          Grand Total : ${grandTotal}
+        </h3>
       </div>
     </div>
   );
